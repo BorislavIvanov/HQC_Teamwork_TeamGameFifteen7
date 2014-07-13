de5e9d4c3889d13c6d947f7260b6a2fd481ed742
BorislavIvanov <borislav.a.ivanov@gmail.com>
2014-07-14 01:43:40 +0300

    Little Refactoring Modifications - 3
    2) Сорс код:
    - Рандом генератора е изведен като „Private static readonly“ поле в
    началото на кода.
    - Метода “static bool check(int i, int j)” e преименуван на “static bool
    CheckIsTheMoveAreLegal(int row, int col)”.
    - Метода „static void Move(int number)“ е преименуван на „static void
    MoveTheNumberOfField(int number)“.
    - Метода „static bool check2()“ e преименуван на „static bool
    IsPuzzleSolved()“.
    - Метода „static void startagain()“ е преименуван на „StartNewGame()“.
    - Кода който се използва за разбъркване на числата в игровото поле
    
    for (int i = 0; i < 1000; i++)
    {
    ………
    {
    
    и се съдържа в методите „StartNewGame()“ и във първият "while“ цикъл
    ползван в Main() е отделен като отделен метод ShuffleThePuzzleField().
    
    3) Преименувани променливи:
    - Променена е променливата „static int [,] a“ на „static int [,]
    puzzleField“.
    - Променена е променливата „bool flag2“ на “bool isTheMoveAreLegal ”,
    която се намира в метода “MoveTheNumberOfField(int number)”.
    - Променена е променливата “bool flagSolved“ на „bool isGameWon“, която
    се намира в Main().
    - Променена е променливата „bool flag“ на „ bool inputIsANumber”, която
    се намира в Main().

--- Game-Fifteen-7_Project/02. Game-Fifteen-7_Refactored/Game15.cs
+++ Game-Fifteen-7_Project/02. Game-Fifteen-7_Refactored/Game15.cs
@@ -7,7 +7,8 @@
 
     class Game15
     {
-        static int[,] a = new int[4, 4] { { 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { 9, 10, 11, 12 }, { 13, 14, 15, 0 } };
+        private static readonly Random randomGenerator = new Random();
+        static int[,] puzzleField = new int[4, 4] { { 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { 9, 10, 11, 12 }, { 13, 14, 15, 0 } };
         static int x = 3;
         static int y = 3;
         static bool flag2 = true;
@@ -15,14 +16,14 @@
         static string[] topPlayersScores = new string[5];
         static int topCount = 0;
 
-        static bool check(int i, int j)
+        static bool CheckIsTheMoveAreLegal(int row, int col)
         {
-            if ((i == x - 1 || i == x + 1) && j == y)
+            if ((row == x - 1 || row == x + 1) && col == y)
             {
                 return true;
             }
 
-            if ((i == x) && (j == y - 1 || j == y + 1))
+            if ((row == x) && (col == y - 1 || col == y + 1))
             {
                 return true;
             }
@@ -30,7 +31,7 @@
             return false;
         }
 
-        static void Move(int number)
+        static void MoveTheNumberOfField(int number)
         {
             int k = x;
             int l = y;
@@ -42,7 +43,7 @@
                 {
                     for (int col = 0; col < 4; col++)
                     {
-                        if (a[row, col] == number)
+                        if (puzzleField[row, col] == number)
                         {
                             k = row;
                             l = col;
@@ -57,17 +58,17 @@
                 }
             }
 
-            bool flag2 = check(k, l);
+            bool isTheMoveAreLegal = CheckIsTheMoveAreLegal(k, l);
 
-            if (!flag2)
+            if (!isTheMoveAreLegal)
             {
                 Console.WriteLine("Illegal move!");
             }
             else
             {
-                int temp = a[k, l];
-                a[k, l] = a[x, y];
-                a[x, y] = temp;
+                int temp = puzzleField[k, l];
+                puzzleField[k, l] = puzzleField[x, y];
+                puzzleField[x, y] = temp;
                 x = k;
                 y = l;
                 countOfTotalMoves++;
@@ -80,17 +81,17 @@
 
                     for (int col = 0; col < 4; col++)
                     {
-                        if (a[row, col] >= 10)
+                        if (puzzleField[row, col] >= 10)
                         {
-                            Console.Write("{0} ", a[row, col]);
+                            Console.Write("{0} ", puzzleField[row, col]);
                         }
-                        else if (a[row, col] == 0)
+                        else if (puzzleField[row, col] == 0)
                         {
                             Console.Write("   ");
                         }
                         else
                         {
-                            Console.Write(" {0} ", a[row, col]);
+                            Console.Write(" {0} ", puzzleField[row, col]);
                         }
                     }
 
@@ -101,21 +102,21 @@
             }
         }
 
-        static bool check2()
+        static bool IsPuzzleSolved()
         {
-            if (a[3, 3] == 0)
+            if (puzzleField[3, 3] == 0)
             {
-                int n = 1;
+                int correctNumber = 1;
 
                 for (int row = 0; row < 4; row++)
                 {
                     for (int col = 0; col < 4; col++)
                     {
-                        if (n <= 15)
+                        if (correctNumber <= 15)
                         {
-                            if (a[row, col] == n)
+                            if (puzzleField[row, col] == correctNumber)
                             {
-                                n++;
+                                correctNumber++;
                             }
                             else
                             {
@@ -133,11 +134,42 @@
             return false;
         }
 
-        static void startagain()
+        static void StartNewGame()
         {
             countOfTotalMoves = 0;
-            Random randomGenerator = new Random();
 
+            ShuffleThePuzzleField();
+
+            Console.WriteLine(" -------------");
+
+            for (int row = 0; row < 4; row++)
+            {
+                Console.Write("| ");
+
+                for (int col = 0; col < 4; col++)
+                {
+                    if (puzzleField[row, col] >= 10)
+                    {
+                        Console.Write("{0} ", puzzleField[row, col]);
+                    }
+                    else if (puzzleField[row, col] == 0)
+                    {
+                        Console.Write("   ");
+                    }
+                    else
+                    {
+                        Console.Write(" {0} ", puzzleField[row, col]);
+                    }
+                }
+
+                Console.WriteLine("|");
+            }
+
+            Console.WriteLine(" -------------");
+        }
+
+        private static void ShuffleThePuzzleField()
+        {
             for (int i = 0; i < 1000; i++)
             {
                 int n = randomGenerator.Next(3);
@@ -149,9 +181,9 @@
 
                     if (nx >= 0 && nx <= 3 && ny >= 0 && ny <= 3)
                     {
-                        int temp = a[x, y];
-                        a[x, y] = a[nx, ny];
-                        a[nx, ny] = temp;
+                        int temp = puzzleField[x, y];
+                        puzzleField[x, y] = puzzleField[nx, ny];
+                        puzzleField[nx, ny] = temp;
                         x = nx;
                         y = ny;
                     }
@@ -169,9 +201,9 @@
 
                     if (nx >= 0 && nx <= 3 && ny >= 0 && ny <= 3)
                     {
-                        int temp = a[x, y];
-                        a[x, y] = a[nx, ny];
-                        a[nx, ny] = temp;
+                        int temp = puzzleField[x, y];
+                        puzzleField[x, y] = puzzleField[nx, ny];
+                        puzzleField[nx, ny] = temp;
                         x = nx;
                         y = ny;
                     }
@@ -189,9 +221,9 @@
 
                     if (nx >= 0 && nx <= 3 && ny >= 0 && ny <= 3)
                     {
-                        int temp = a[x, y];
-                        a[x, y] = a[nx, ny];
-                        a[nx, ny] = temp;
+                        int temp = puzzleField[x, y];
+                        puzzleField[x, y] = puzzleField[nx, ny];
+                        puzzleField[nx, ny] = temp;
                         x = nx;
                         y = ny;
                     }
@@ -209,9 +241,9 @@
 
                     if (nx >= 0 && nx <= 3 && ny >= 0 && ny <= 3)
                     {
-                        int temp = a[x, y];
-                        a[x, y] = a[nx, ny];
-                        a[nx, ny] = temp;
+                        int temp = puzzleField[x, y];
+                        puzzleField[x, y] = puzzleField[nx, ny];
+                        puzzleField[nx, ny] = temp;
                         x = nx;
                         y = ny;
                     }
@@ -221,33 +253,6 @@
                     }
                 }
             }
-
-            Console.WriteLine(" -------------");
-
-            for (int row = 0; row < 4; row++)
-            {
-                Console.Write("| ");
-
-                for (int col = 0; col < 4; col++)
-                {
-                    if (a[row, col] >= 10)
-                    {
-                        Console.Write("{0} ", a[row, col]);
-                    }
-                    else if (a[row, col] == 0)
-                    {
-                        Console.Write("   ");
-                    }
-                    else
-                    {
-                        Console.Write(" {0} ", a[row, col]);
-                    }
-                }
-
-                Console.WriteLine("|");
-            }
-
-            Console.WriteLine(" -------------");
         }
 
         static void move(int i, string res)
@@ -270,90 +275,8 @@
             while (flag2)
             {
                 countOfTotalMoves = 0;
-                Random r = new Random();
 
-                for (int i = 0; i < 1000; i++)
-                {
-                    int n = r.Next(3);
-
-                    if (n == 0)
-                    {
-                        int nx = x - 1;
-                        int ny = y;
-                        if (nx >= 0 && nx <= 3 && ny >= 0 && ny <= 3)
-                        {
-                            int temp = a[x, y];
-                            a[x, y] = a[nx, ny];
-                            a[nx, ny] = temp;
-                            x = nx;
-                            y = ny;
-                        }
-                        else
-                        {
-                            n++;
-                            i--;
-                        }
-                    }
-
-                    if (n == 1)
-                    {
-                        int nx = x;
-                        int ny = y + 1;
-
-                        if (nx >= 0 && nx <= 3 && ny >= 0 && ny <= 3)
-                        {
-                            int temp = a[x, y];
-                            a[x, y] = a[nx, ny];
-                            a[nx, ny] = temp;
-                            x = nx;
-                            y = ny;
-                        }
-                        else
-                        {
-                            n++;
-                            i--;
-                        }
-                    }
-
-                    if (n == 2)
-                    {
-                        int nx = x + 1;
-                        int ny = y;
-
-                        if (nx >= 0 && nx <= 3 && ny >= 0 && ny <= 3)
-                        {
-                            int temp = a[x, y];
-                            a[x, y] = a[nx, ny];
-                            a[nx, ny] = temp;
-                            x = nx;
-                            y = ny;
-                        }
-                        else
-                        {
-                            n++;
-                            i--;
-                        }
-                    }
-
-                    if (n == 3)
-                    {
-                        int nx = x;
-                        int ny = y - 1;
-
-                        if (nx >= 0 && nx <= 3 && ny >= 0 && ny <= 3)
-                        {
-                            int temp = a[x, y];
-                            a[x, y] = a[nx, ny];
-                            a[nx, ny] = temp;
-                            x = nx;
-                            y = ny;
-                        }
-                        else
-                        {
-                            i--;
-                        }
-                    }
-                }
+                ShuffleThePuzzleField();
 
                 Console.WriteLine("Welcome to the game “15”.\nPlease try to arrange the numbers sequentially.\nUse 'top' to view the top scoreboard,\n'restart' to start a new game\nand 'exit' to quit the game.\n");
                 Console.WriteLine(" -------------");
@@ -364,17 +287,17 @@
 
                     for (int col = 0; col < 4; col++)
                     {
-                        if (a[row, col] >= 10)
+                        if (puzzleField[row, col] >= 10)
                         {
-                            Console.Write("{0} ", a[row, col]);
+                            Console.Write("{0} ", puzzleField[row, col]);
                         }
-                        else if (a[row, col] == 0)
+                        else if (puzzleField[row, col] == 0)
                         {
                             Console.Write("   ");
                         }
                         else
                         {
-                            Console.Write(" {0} ", a[row, col]);
+                            Console.Write(" {0} ", puzzleField[row, col]);
                         }
                     }
 
@@ -383,20 +306,20 @@
 
                 Console.WriteLine(" -------------");
 
-                bool flagSolved = check2();
+                bool isGameWon = IsPuzzleSolved();
 
-                while (!flagSolved)
+                while (!isGameWon)
                 {
                     Console.Write("Enter a number to move: ");
                     string inputCommand = Console.ReadLine();
                     int selectedNumber;
-                    bool flag = int.TryParse(inputCommand, out selectedNumber);
+                    bool inputIsANumber = int.TryParse(inputCommand, out selectedNumber);
 
-                    if (flag)
+                    if (inputIsANumber)
                     {
                         if (selectedNumber >= 1 && selectedNumber <= 15)
                         {
-                            Move(selectedNumber);
+                            MoveTheNumberOfField(selectedNumber);
                         }
                         else
                         {
@@ -415,7 +338,7 @@
                         {
                             if (inputCommand == "restart")
                             {
-                                startagain();
+                                StartNewGame();
                             }
                             else
                             {
@@ -444,10 +367,10 @@
                             }
                         }
                     }
-                    flagSolved = check2();
+                    isGameWon = IsPuzzleSolved();
                 }
 
-                if (flagSolved)
+                if (isGameWon)
                 {
                     Console.WriteLine("Congratulations! You won the game in {0} moves.", countOfTotalMoves);
 
