b930abe2ad0c9c37622f01b5cbcdb61108ed168b
Dr4g0 <d_d0003@yahoo.com>
2014-07-23 21:40:56 +0300

    Add ProgramClassTests.cs

---
+++ Game-Fifteen-7_Project/03. Game-Fifteen-7_Tests/GameEngineClassTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GameFifteenVersionSeven;
+
+namespace GameFifteenVersionSevenTests
+{
+    [TestClass]
+    public class GameEngineClassTests
+    {
+        [TestMethod]
+        public void ShouldReturnTrue_DefineCommandsMethodTest()
+        {
+            Player testPlayer=new Player();
+            GameEngine testEngine=new GameEngine(testPlayer);
+            List<Player> testTopPlayers=new List<Player>();
+        }
+    }
+}
---
+++ Game-Fifteen-7_Project/03. Game-Fifteen-7_Tests/ProgramClassTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using GameFifteenVersionSeven;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GameFifteenVersionSevenTests
+{
+    [TestClass]
+    public class ProgramClassTests
+    {
+        
+        [TestMethod]
+        public void ShouldReturnTrue_CheckIsIncreasingTotalPlayerMovesOrPrintSpecificMessage()
+        {
+            Player testPlayer = new Player();
+            GameEngine testEngine = new GameEngine(testPlayer);
+            var oldIn = Console.In;
+            Console.SetIn(new StringReader("5\r\nexit"));
+            string result = String.Empty;
+            using (var writer = new StringWriter())
+            {
+                Console.SetOut(writer);
+                testEngine.StartTheGame();
+
+                writer.Flush();
+
+                result = writer.GetStringBuilder().ToString();
+            }
+            Console.SetIn(oldIn);
+            int expectedPlayerMoves = 1;
+            string expectedMessage = "Illegal move!\r\nEnter a number to move: Good bye!\r\n";
+            string resultString = result.Substring(result.Length - 50);
+            Assert.IsTrue(expectedPlayerMoves == testPlayer.TotalMoves || expectedMessage == resultString);
+
+        }
+    }
+}
